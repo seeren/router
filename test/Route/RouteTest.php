2c9a0e3328c63490ff51d26e428b14205f852091
Cyril <consultant@seeren.fr>
2017-08-05 17:22:46 +0200

    Cover Route and Router

--- test/Route/RouteTest.php
+++ test/Route/RouteTest.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * This file contain Seeren\Router\Test\Route\RouteInterfaceTest class
+ * This file contain Seeren\Router\Test\Route\RouteTest class
  *     __
  *    / /__ __ __ __ __ __
  *   / // // // // // // /
@@ -15,8 +15,8 @@
 
 namespace Seeren\Router\Test\Route;
 
-use Seeren\Router\Route\Route;
 use Seeren\Router\Route\RouteInterface;
+use Seeren\Router\Route\Route;
 use ReflectionClass;
 
 /**
@@ -25,9 +25,8 @@ use ReflectionClass;
  * @category Seeren
  * @package Router
  * @subpackage Test\Route
- * @abstract
  */
-class RouteTest extends RouteInterfaceTest
+class RouteTest extends AbstractRouteTest
 {
 
    /**
@@ -35,9 +34,120 @@ class RouteTest extends RouteInterfaceTest
     * 
     * @return RouteInterface route
     */
-   protected function getRouteInterface(): RouteInterface
+    protected function getRoute(): RouteInterface
+    {
+        return (new ReflectionClass(Route::class))->newInstanceArgs([]);
+    }
+
+    /**
+     * @covers \Seeren\Router\Route\Route::__construct
+     * @covers \Seeren\Router\Route\Route::setAction
+     * @covers \Seeren\Router\Route\Route::setController
+     * @covers \Seeren\Router\Route\Route::setParam
+     * @covers \Seeren\Router\Route\Route::setPath
+     * @covers \Seeren\Router\Route\Route::setPrefix
+     * @covers \Seeren\Router\Route\Route::__toString
+     */
+    public function test__toString()
+    {
+        $this->assertTrue(
+            (string) $this->getRoute()
+            ->setAction("get")
+            ->setController("DummyController")
+            ->setParam([true])
+            ->setPath("/")
+            ->setPrefix("DummyPrefix")
+        === "DummyPrefix\Controller\\DummyController"
+        );
+    }
+
+    /**
+     * @covers \Seeren\Router\Route\Route::__construct
+     * @covers \Seeren\Router\Route\Route::clone
+     */
+    public function testClone()
+    {
+        parent::testClone();
+    }
+
+   /**
+    * @covers \Seeren\Router\Route\Route::__construct
+    * @covers \Seeren\Router\Route\Route::setAction
+    * @covers \Seeren\Router\Route\Route::getAction
+    */
+   public function testSetAction()
    {
-       return (new ReflectionClass(Route::class))->newInstanceArgs([]);
+       parent::testSetAction();
+   }
+
+   /**
+    * @covers \Seeren\Router\Route\Route::__construct
+    * @covers \Seeren\Router\Route\Route::setPrefix
+    * @covers \Seeren\Router\Route\Route::getPrefix
+    */
+   public function testSetPrefix()
+   {
+       parent::testSetPrefix();
+   }
+
+   /**
+    * @covers \Seeren\Router\Route\Route::__construct
+    * @covers \Seeren\Router\Route\Route::setPrefix
+    * @expectedException \InvalidArgumentException
+    */
+   public function testSetPrefixInvalidArgumentException()
+   {
+       parent::testSetPrefixInvalidArgumentException();
+   }
+
+   /**
+    * @covers \Seeren\Router\Route\Route::__construct
+    * @covers \Seeren\Router\Route\Route::setController
+    * @covers \Seeren\Router\Route\Route::getController
+    */
+   public function testSetController()
+   {
+       parent::testSetController();
+   }
+
+   /**
+    * @covers \Seeren\Router\Route\Route::__construct
+    * @covers \Seeren\Router\Route\Route::setController
+    * @expectedException \InvalidArgumentException
+    */
+   public function testSetControllerInvalidArgumentException()
+   {
+       parent::testSetControllerInvalidArgumentException();
+   }
+
+   /**
+    * @covers \Seeren\Router\Route\Route::__construct
+    * @covers \Seeren\Router\Route\Route::setParam
+    * @covers \Seeren\Router\Route\Route::getParam
+    */
+   public function testSetParam()
+   {
+       parent::testSetParam();
+   }
+
+   /**
+    * @covers \Seeren\Router\Route\Route::__construct
+    * @covers \Seeren\Router\Route\Route::setPath
+    * @covers \Seeren\Router\Route\Route::getPath
+    */
+   public function testSetPath()
+   {
+       parent::testSetPath();
+   }
+
+   /**
+    * @covers \Seeren\Router\Route\Route::__construct
+    * @covers \Seeren\Router\Route\Route::setPath
+    * @expectedException \InvalidArgumentException
+    */
+   public function testSetPathInvalidArgumentException()
+   {
+       parent::testSetPathInvalidArgumentException();
    }
 
 }
