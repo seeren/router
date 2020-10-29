b0743ad246d92baa719777e42d61578bcec5ea0c
seeren <consultant@seeren.fr>
2020-10-29 16:12:00 +0100

    Kill src and test

--- src/Route/RouteInterface.php
+++ src/Route/RouteInterface.php
@@ -1,148 +1,49 @@
 <?php
 
+namespace Seeren\Router\Route;
+
 /**
+ * Interface to represent a route
+ *
  *     __
  *    / /__ __ __ __ __ __
  *   / // // // // // // /
  *  /_// // // // // // /
  *    /_//_//_//_//_//_/
  *
- * @author (c) Cyril Ichti <consultant@seeren.fr>
- * @link https://github.com/seeren/router
- * @version 1.0.2
- */
-namespace Seeren\Router\Route;
-
-/**
- * Interface for represente a route
- *
- * @category Seeren
- * @package Router
- * @subpackage Route
+ * @package Seeren\Router\Route
  */
 interface RouteInterface
 {
 
-    const
-
-        /**
-         * @var string
-         */
-        ACTION = "action",
-        
-        /**
-         * @var string
-         */
-        PREFIX = "prefix",
-        
-        /**
-         * @var string
-         */
-        CONTROLLER = "controller", /**
-    
-        /**
-         * @var string
-         */
-        PARAM = "param", /**
-    
-        /**
-         * @var string
-         */
-        PATH = "path";
-
     /**
-     * Get instance
-     *
-     * @return RouteInterface instance     
-     */
-    public function clone(): RouteInterface;
-
-    /**
-     * Get action
-     *
-     * @return string action
-     */
-    public function getAction(): string;
-
-    /**
-     * Set action
-     *
-     * @param string $action
-     * @return RouteInterface
-     */
-    public function setAction(string $action): RouteInterface;
-
-    /**
-     * Get prefix
-     *
-     * @return string prefix
-     */
-    public function getPrefix(): string;
-
-    /**
-     * Set prefix
-     *
-     * @param string $prefix
-     * @return RouteInterface
-     *        
-     * @throws \InvalidArgumentException for invalid prefix
-     */
-    public function setPrefix(string $prefix): RouteInterface;
-
-    /**
-     * Get controller
-     *
-     * @return string controller
-     */
-    public function getController(): string;
-
-    /**
-     * Set controller
-     *
-     * @param string $controller
-     * @return RouteInterface
-     *        
-     * @throws \InvalidArgumentException for invalid controller
-     */
-    public function setController(string $controller): RouteInterface;
-
-    /**
-     * Get param
-     *
-     * @return string param
-     */
-    public function getParam(): array;
-
-    /**
-     * Set param
-     *
-     * @param array $param
-     * @return RouteInterface
-     */
-    public function setParam(array $param): RouteInterface;
-
-    /**
-     * Get path
-     *
-     * @return string path
+     * @return string
      */
     public function getPath(): string;
 
     /**
-     * Set path
-     *
-     * @param string $path
-     * @return RouteInterface
-     *        
-     * @throws \InvalidArgumentException for invalid path
+     * @return string
      */
-    public function setPath(string $path): RouteInterface;
+    public function getAction(): string;
 
     /**
-     * To string
-     *
-     * @return string namespace
+     * @return string
      */
-    public function __toString(): string;
+    public function getController(): string;
+
+    /**
+     * @return array
+     */
+    public function getMethods(): array;
+
+    /**
+     * @return array
+     */
+    public function getSlugs(): array;
+
+    /**
+     * @param array $slugs
+     */
+    public function setSlugs(array $slugs): void;
 
 }
