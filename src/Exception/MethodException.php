6ca7173044f9a5c5e2d0bbd253241d93457cc25d
seeren <consultant@seeren.fr>
2020-11-01 15:00:36 +0100

    Create exceptions

---
+++ src/Exception/MethodException.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Seeren\Router\Exception;
+
+use Exception;
+
+/**
+ * Class to represent a method exception
+ *
+ *     __
+ *    / /__ __ __ __ __ __
+ *   / // // // // // // /
+ *  /_// // // // // // /
+ *    /_//_//_//_//_//_/
+ *
+ * @package Seeren\Router\Exception
+ */
+class MethodException extends Exception
+{
+}
---
+++ src/Exception/RouteException.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Seeren\Router\Exception;
+
+use Exception;
+
+/**
+ * Class to represent a route exception
+ *
+ *     __
+ *    / /__ __ __ __ __ __
+ *   / // // // // // // /
+ *  /_// // // // // // /
+ *    /_//_//_//_//_//_/
+ *
+ * @package Seeren\Router\Exception
+ */
+class RouteException extends Exception
+{
+}
